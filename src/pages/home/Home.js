542431136af0f645d804d895a70fdcad3f0ad93d
mariana-a-ribeiro <mariana.a.ribeiro1@gmail.com>
2019-04-10 17:04:57 -0300

    ajuste

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react';
 import {Text, Button, View, Image} from 'react-native';
 
 
-export default class App extends Component{
+export default class Home extends Component{
     constructor(props){
         super(props);
 
